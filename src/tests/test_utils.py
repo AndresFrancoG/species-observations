2311305666988b9befb1bd102ebaf7169b179d43
Andres <anfefra@gmail.com>
2023-05-24 09:43:31 -0400

    ADD: Unit test for the function PartitionedDS2df File src/species_observations/utils.py Function PartitionedDS2df Checks if a dataframe is loaded, and if the columns of each individual datasets of the original partitioned data are equal to the columns of the resulting dataframe

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -12,3 +12,14 @@ def test_load_partitionedDS_kedro():
     assert type(ds_dict) == expected_type
     for k,v in ds_dict.items():
         assert type(v()) == expected_item_type
+
+
+def test_PartitionedDS2df():
+    path = "data//01_raw//species_bigQuery_sample"
+    dataset = CSVDataSet
+    expected_type = pd.DataFrame
+    ds_dict = utl.load_partitionedDS_kedro(path, dataset)
+    df = utl.PartitionedDS2df(ds_dict)
+    assert type(df) == expected_type
+    for k,v in ds_dict.items():
+        assert (v().columns == df.columns).all()
