d19310915b3d6276a769c9223168341a8525f084
Andres <anfefra@gmail.com>
2023-05-24 09:33:06 -0400

    ADD: Unit test for load_partitionedDS_kedro() Unit tests for src/species_observations/utils.py Function load_partitionedDS_kedro() Checks that a dictionary is loaded properly from partitioned dataset Checks that each individual item of the loaded dictionary is of type pd.DataFrame

--- src/species_observations/utils.py
+++ src/species_observations/utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from typing import Dict
+from kedro.io import PartitionedDataSet
 
 
 def PartitionedDS2df(pd_dict: Dict) -> pd.DataFrame:
@@ -22,3 +23,27 @@ def PartitionedDS2df(pd_dict: Dict) -> pd.DataFrame:
             [df, partition_data], ignore_index=True, sort=True
         )
     return df
+
+def load_partitionedDS_kedro(path: str, dataset: Dict) -> pd.DataFrame:
+    """Loads a partitioned data stored in the folder specified in path.
+
+    Parameters
+    ----------
+    path : str
+        Folder of the partitioned data
+    dataset : Dict
+        Type of data to search for and load options (dataset option in kedro's catalog of type PartitionedDataSet)
+
+    Returns
+    -------
+    pd.DataFrame
+        joined dataset
+    """
+    data_set = PartitionedDataSet(
+        path=path,
+        dataset=dataset,
+    )
+
+    print(dataset)
+
+    return data_set.load()
--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -1,2 +1,14 @@
 import pytest
+import pandas as pd
+from kedro_datasets.pandas import CSVDataSet
 import species_observations.utils as utl
+
+def test_load_partitionedDS_kedro():
+    path = "data//01_raw//species_bigQuery_sample"
+    dataset = CSVDataSet
+    expected_type = dict
+    expected_item_type = pd.DataFrame
+    ds_dict = utl.load_partitionedDS_kedro(path, dataset)
+    assert type(ds_dict) == expected_type
+    for k,v in ds_dict.items():
+        assert type(v()) == expected_item_type
