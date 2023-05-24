c19e9600d6489057e5da6f7a2a04cbba2cd8273b
Andres <anfefra@gmail.com>
2023-05-24 11:19:23 -0400

    ADD: Documentation of class Preprocessing()

--- src/species_observations/scripts/data_processing.py
+++ src/species_observations/scripts/data_processing.py
@@ -1,19 +1,46 @@
 import pandas as pd
 import species_observations.utils as utl
 
-class Preprocessing:
-    def __init__(self, parameters):
-        self.full_cols = parameters['data_cols']
-        self.date_col = self.full_cols['event_date']
-        self.count_col = self.full_cols['individual_count']
+from typing import Dict
 
-        self.preproc_params = parameters['preprocessing']
-        self.resample = self.preproc_params['resampling_period']
+class Preprocessing:
+    def __init__(self, parameters: Dict):
+        """Defines the parameters necessary for data preprocessing in the current project.
+
+        Parameters
+        ----------
+        parameters : Dict
+            Dictionary with parameters loaded through kedro.
+        """
+        self._full_cols = parameters['data_cols']
+        self._date_col = self._full_cols['event_date']
+        self._count_col = self._full_cols['individual_count']
+
+        self._preproc_params = parameters['preprocessing']
+        self._resample = self._preproc_params['resampling_period']
+
+        self._datetime_suffix = '_datetime'
 
     def preprocessing_time_data(self, df: pd.DataFrame) -> pd.DataFrame:
-        date_col = self.date_col
-        date_col_datetime = date_col + '_datetime'
-        count_col = self.count_col
+        """Fills NaN in the column count_col with zeros.  
+            Takes the column date_col of type string and transforms it to datetime. 
+            Stores it in a new column of name date_col + "_datetime"
+            count_col and date_col are defined at the constructor through 'parameters'
+
+        Parameters
+        ----------
+        df : pd.DataFrame or dict of dataframes loaded from partitioned data
+            Info to be processed.  If dict, first concatenates all entries into 
+            a single df
+
+        Returns
+        -------
+        pd.DataFrame
+            Processed datafram
+        """
+        date_col = self._date_col
+        date_col_datetime = date_col + self._datetime_suffix
+        count_col = self._count_col
 
         if type(df) == dict:
             df = utl.PartitionedDS2df(df)
@@ -25,7 +52,22 @@ class Preprocessing:
         return df_out
     
     def time_resampling(self, df: pd.DataFrame) -> pd.DataFrame:
-        date_col = 'eventdate'
-        date_col_datetime = date_col + '_datetime'
-        resample = self.resample
+        """Resamples df to the frequency specified by "resample".
+            Used the column date_col_datetime as basis for the resample,
+            and sets the resampled date_col_datetime as index
+            "date_col" and "resample" are defined at the constructor through 'parameters'
+
+        Parameters
+        ----------
+        df : pd.DataFrame
+            DataFrame to be resampled
+
+        Returns
+        -------
+        pd.DataFrame
+            Resample dataframe
+        """
+        date_col = self._date_col
+        date_col_datetime = date_col + self._datetime_suffix
+        resample = self._resample
         return df.set_index(date_col_datetime).resample(resample).sum()
--- src/tests/pipelines/scripts/test_data_processing.py
+++ src/tests/pipelines/scripts/test_data_processing.py
@@ -1,31 +1,3 @@
-import pandas as pd
 import species_observations.utils as utl
+import species_observations.scripts.data_processing as dtp
 
-class Preprocessing:
-    def __init__(self, parameters):
-        self.full_cols = parameters['data_cols']
-        self.date_col = self.full_cols['event_date']
-        self.count_col = self.full_cols['individual_count']
-
-        self.preproc_params = parameters['preprocessing']
-        self.resample = self.preproc_params['resampling_period']
-
-    def preprocessing_time_data(self, df: pd.DataFrame) -> pd.DataFrame:
-        date_col = self.date_col
-        date_col_datetime = date_col + '_datetime'
-        count_col = self.count_col
-
-        if type(df) == dict:
-            df = utl.PartitionedDS2df(df)
-
-        df[date_col_datetime] = pd.to_datetime(df[date_col])
-        df[count_col] = df[count_col].fillna(0)
-        df_out = df[[count_col, date_col_datetime]]
-
-        return df_out
-    
-    def time_resampling(self, df: pd.DataFrame) -> pd.DataFrame:
-        date_col = 'eventdate'
-        date_col_datetime = date_col + '_datetime'
-        resample = self.resample
-        return df.set_index(date_col_datetime).resample(resample).sum()
