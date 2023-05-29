3e614be4cb4262a74eae13b75aeeef54f3d0fadb
Andres <anfefra@gmail.com>
2023-05-29 12:27:36 -0400

    REF: Refactors load_csv_from_catalog Changes output type to csv. Adds load_csv_from_filepath helper function which loads csv from a path relative to the kedro project Make the appropriate changes to previous test cases

--- src/tests/pipelines/observations_time/test_nodes.py
+++ src/tests/pipelines/observations_time/test_nodes.py
@@ -1,9 +1,19 @@
 """
-This is a boilerplate pipeline 'observations_time'
-generated using Kedro 0.18.8
+Unit tests for node functions of pipeline observations_time
 """
-# import pytest
-# import species_observations.pipelines.observations_time.nodes as nd
+import pytest
 
-# def test_preprocessing_time_data(test_input, expected):
-#     assert eval(test_input) == expected
+import species_observations.utils as utl
+import species_observations.pipelines.observations_time.nodes as nd
+
+@pytest.mark.parametrize(
+        ('kedro_env', 'catalog_entry'),
+        [
+            ('test_cloud', 'preprocessing')
+    ])
+def node_preprocessing_time_data(kedro_env: str, catalog_entry: str):
+    config = utl.load_config_file_kedro(kedro_env = kedro_env)
+    parameters = config['parameters']
+
+    df_sample = utl.load_csv_from_catalog(kedro_env, config_entry=catalog_entry)
+    df_resampled = nd.node_preprocessing_time_data(df_sample, parameters)
