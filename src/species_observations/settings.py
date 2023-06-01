042fb47df4f9d2a75f637434232db988f4751107
Andres <anfefra@gmail.com>
2023-06-01 16:39:55 -0400

    FIX: Missing config in settings.py

--- src/species_observations/settings.py
+++ src/species_observations/settings.py
@@ -33,6 +33,10 @@ https://kedro.readthedocs.io/en/stable/kedro_project_setup/settings.html."""
 #           "parameters": ["parameters*", "parameters*/**", "**/parameters*"],
 #       }
 # }
+from kedro.config import TemplatedConfigLoader  # new import
+
+CONFIG_LOADER_CLASS = TemplatedConfigLoader
+
 CONFIG_LOADER_ARGS = {"globals_pattern": "*globals.yml"}
 # Class that manages the Data Catalog.
 # from kedro.io import DataCatalog
